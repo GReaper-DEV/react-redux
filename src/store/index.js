411c70842fd6de135385208eeb7a931b883765cd
Ioannis Kotsonis <johnkotsgr@gmail.com>
2024-11-25 15:57:10 +0200

    Remove redundant code.

--- src/store/index.js
+++ src/store/index.js
@@ -1,47 +1,14 @@
-import {configureStore, createSlice} from '@reduxjs/toolkit';
-
-
-const songsSlice = createSlice({
-    name: 'song',
-    initialState: [],
-    reducers: {
-        addSong(state, action) {
-            state.push(action.payload)
-        },
-        removeSong(state, action) {
-            const index = state.indexOf(action.payload);
-            if (index === undefined || index === null) {
-                throw new Error(`The ${action.payload} does not exist in the store`)
-            }
-            state.splice(index, 1)
-        }
-
-    }
-});
-const moviesSlice = createSlice({
-    name: 'movies',
-    initialState: [],
-    reducers: {
-        addMovie(state, action) {
-            state.push(action.payload)
-        },
-        removeMovie(state, action) {
-            const index = state.indexOf(action.payload);
-            if (index === undefined || index === null) {
-                throw new Error(`The ${action.payload} does not exist in the store`)
-            }
-            state.splice(index, 1)
-        }
-    }
-});
+import {configureStore} from '@reduxjs/toolkit';
+import {moviesReducer,addMovie, removeMovie} from './slices/MoviesSlice.js';
+import {songsReducer, addSong, removeSong} from './slices/SongsSlice.js';
+import {reset} from './actions';
 
 const store = configureStore({
     reducer: {
-        songs: songsSlice.reducer,
-        movies: moviesSlice.reducer
+        songs: songsReducer,
+        movies: moviesReducer
     }
 });
 
-export const {addSong, removeSong} = songsSlice.actions;
-export const {addMovie, removeMovie} = moviesSlice.actions;
 export {store};
+export {reset, addMovie, removeMovie, addSong, removeSong};
